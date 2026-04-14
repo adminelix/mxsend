4da59e12eb763bf2b5c45d296f3f4e1c770dbe7d
adminelix <4dm1n3l1x@mailbox.org>
2026-04-14 19:36:02 +0200

    feat: switch to a data_dir with default

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,25 @@ use matrix_sdk::{Client, Room, RoomMemberships, config::SyncSettings};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use tokio::fs;
-use url::Url;
+
+fn default_data_dir() -> PathBuf {
+    dirs::data_dir()
+        .unwrap_or_else(|| {
+            eprintln!("Error: Could not determine data directory. Please specify --data-directory");
+            std::process::exit(1);
+        })
+        .join(env!("CARGO_PKG_NAME"))
+}
 
 #[derive(Parser, Debug)]
 struct Cli {
+    /// The Data Directory to store session, with default in
+    /// Linux: $XDG_DATA_HOME or $HOME/.local/share
+    /// Windows {FOLDERID_RoamingAppData}
+    /// MacOs: $HOME/Library/Application Support
+    #[clap(long = "data-directory", default_value_os_t = default_data_dir())]
+    data_dir: PathBuf,
+
     /// The Matrix user ID of the sender
     #[clap(long = "sender-id")]
     sender_id: String,
@@ -18,10 +33,6 @@ struct Cli {
     /// The Matrix user ID of the recipient
     #[clap(long = "recipient-id")]
     recipient_id: String,
-
-    /// The Matrix homeserver base URL
-    #[clap(long = "host", default_value = "https://matrix.org")]
-    host: Url,
 
     /// The sender's Matrix account password
     #[clap(long = "password")]
@@ -56,10 +67,10 @@ async fn main() -> anyhow::Result<()> {
         .build()
         .await?;
 
-    let session_file_path = session_file_path();
+    let session_file_path = foo(&cli.data_dir).await;
 
     if session_file_path.exists() {
-        match restore_session(&client).await {
+        match restore_session(&client, &session_file_path).await {
             Ok(sync_token) => {
                 sync_settings = sync_settings.token(sync_token);
             }
@@ -80,7 +91,7 @@ async fn main() -> anyhow::Result<()> {
             .expect("Logged-in client should have a session"),
         sync_token: next_sync_token,
     };
-    write_session(&session).await?;
+    write_session(&session_file_path, &session).await?;
 
     let room = determine_room(&recipient_id, &client).await?;
     let content = RoomMessageEventContent::text_plain(&cli.message);
@@ -124,33 +135,34 @@ async fn determine_room(recipient_id: &OwnedUserId, client: &Client) -> anyhow::
 }
 
 /// Restores a previous session
-async fn restore_session(client: &Client) -> anyhow::Result<String> {
-    let session = read_session().await?;
+async fn restore_session(client: &Client, path: &PathBuf) -> anyhow::Result<String> {
+    let session = read_session(path).await?;
     client.restore_session(session.user_session).await?;
     println!("Session restored successfully");
 
     Ok(session.sync_token)
 }
 
-async fn read_session() -> anyhow::Result<FullSession> {
-    let session_file_path = session_file_path();
-    let serialized_session = fs::read_to_string(session_file_path).await?;
+async fn read_session(file_path: &PathBuf) -> anyhow::Result<FullSession> {
+    let serialized_session = fs::read_to_string(file_path).await?;
     let session: FullSession = serde_json::from_str(&serialized_session)?;
     Ok(session)
 }
 
-async fn write_session(session: &FullSession) -> anyhow::Result<()> {
-    let session_file_path = session_file_path();
+async fn write_session(file_path: &PathBuf, session: &FullSession) -> anyhow::Result<()> {
+    if let Some(parent) = file_path.parent() {
+        fs::create_dir_all(parent).await?;
+    }
+
     let serialized_session = serde_json::to_string_pretty(session)?;
-    fs::write(session_file_path, serialized_session).await?;
+    fs::write(file_path, serialized_session).await?;
     Ok(())
 }
 
-fn session_file_path() -> PathBuf {
-    std::env::current_exe()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .to_path_buf()
-        .join("session.json")
+async fn foo(data_dir: &PathBuf) -> PathBuf {
+    let mut clone = data_dir.clone();
+    if !clone.ends_with(env!("CARGO_PKG_NAME")) {
+        clone.push(env!("CARGO_PKG_NAME"));
+    }
+    clone.join("session.json")
 }
