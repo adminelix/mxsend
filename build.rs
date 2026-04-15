30c6a10caf734fe1babe2bbe55176bcba6e90b16
adminelix <4dm1n3l1x@mailbox.org>
2026-04-15 16:13:13 +0200

    refactor: improve session management and env variable usage in build script and main code

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let pkg_name = std::env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME no found");
-    let prefix = format!("{}_", pkg_name.to_uppercase().replace("-", "_"));
-
-    println!("cargo:rustc-env=CARGO_PKG_NAME_UPPERCASE={}", prefix);
-
+    println!(
+        "cargo:rustc-env=CARGO_PKG_NAME_UPPERCASE={}",
+        env!("CARGO_PKG_NAME").to_uppercase().replace("-", "_")
+    );
     println!("cargo:rerun-if-changed=Cargo.toml");
 }
